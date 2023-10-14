0af22685f78557206207f756ab3c017eb7e2cd68
Daryl Lee <daryl@daryllee.com>
2023-10-14 17:58:02 -0600

    Ready for Dec. 1

---
+++ template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 08:45:12 2023
+
+@author: Daryl Lee
+"""
+
+filename = 'input.txt'
+def main():
+    f = open(filename)
+    lines = f.readlines()
+    
+    
+    # Daily code goes here
+    
+    
+    return 0
+
+if __name__ == '__main__':
+    main()
+    
